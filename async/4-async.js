a8bc018697c16e0c8b8804100b4e1e4bb4ed5e9d
James Speirs <james.speirs@gmail.com>
2017-02-17 22:08:02 -0700

    rename files

--- async/4-async.js
+++ async/4-async.js
@@ -5,26 +5,22 @@ const path = require('path');
 const small = path.resolve(__dirname, './files/small.txt');
 const medium = path.resolve(__dirname, './files/medium.txt');
 const large = path.resolve(__dirname, './files/large.txt');
-let done = 3;
-
-function checkDone() {
-    done--;
-    if (done === 0) console.log('Total: ' + (Date.now() - t0));
-}
 
 const t0 = Date.now();
 
 fs.readFile(small, function(err, data) {
-    console.log('Small: ' + (Date.now() - t0));
-    checkDone();
-});
+    const t1 = Date.now();
+    console.log('Small: ' + (t1 - t0));
 
-fs.readFile(medium, function(err, data) {
-    console.log('Medium: ' + (Date.now() - t0));
-    checkDone();
-});
+    fs.readFile(medium, function(err, data) {
+        const t2 = Date.now();
+        console.log('Medium: ' + (t2 - t1));
 
-fs.readFile(large, function(err, data) {
-    console.log('Large: ' + (Date.now() - t0));
-    checkDone();
+        fs.readFile(large, function(err, data) {
+            const t3 = Date.now();
+            console.log('Large: ' + (t3 - t2));
+
+            console.log('Total: ' + (t3 - t0));
+        });
+    });
 });
