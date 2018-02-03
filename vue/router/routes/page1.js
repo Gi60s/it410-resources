d843f96db03ea71ed5ea64539ce0b4ab90f99427
James Speirs <james.speirs@gmail.com>
2018-02-02 23:27:03 -0700

    add vue router example

---
+++ vue/router/routes/page1.js
@@ -0,0 +1,5 @@
+if (!window.components) window.components = {};
+
+components.page1 = {
+    template: `<div><h1>Page 1</h1></div>`
+};
---
+++ vue/router/routes/page2.js
@@ -0,0 +1,6 @@
+if (!window.components) window.components = {};
+
+components.page2 = {
+    template: `<div><h1>Page 2</h1></div>`
+};
+
