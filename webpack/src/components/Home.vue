023ad6e046cdf2abb9448daba06ca29d54b3cedc
James Speirs <james.speirs@gmail.com>
2018-02-10 13:42:14 -0700

    create webpack vue example

---
+++ webpack/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home Page</h2>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        message: 'This is the home page'
+    }
+}
+
+</script>
+
+<style scoped>
+    p {
+        font-weight: bold;
+        color: orange;
+    }
+</style>
---
+++ webpack/webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+const path = require('path');
+module.exports = {
+    entry: { 
+        app: path.resolve(__dirname, 'src/app.js')
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dest')
+    },
+
+    context: path.resolve(__dirname),
+
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" }, // creates style nodes from JS strings
+                    { loader: "css-loader" },   // translates CSS into CommonJS
+                    { loader: "sass-loader" }   // compiles Sass to CSS
+                ]
+            },
+
+            // vue loader: https://vue-loader.vuejs.org/en/
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'            // converts .vue files into HTML, JavaScript, CSS
+            }
+        ]
+    }
+};
